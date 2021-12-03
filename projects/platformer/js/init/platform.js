9712fbf66403a3c6783736c266b02f2aae90e003
samanthajones1 <sammy.l.j2004@gmail.com>
2021-12-03 16:08:22 +0000

    add changes to the game

---
+++ projects/platformer/js/init/platform.js
@@ -0,0 +1,51 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.platform = window.opspark.platform || {};
+    
+    let platform = window.opspark.platform;
+    
+    /**
+     * init: This function initializes the platforms for the level.
+     * 
+     * GOAL: Add as many platforms necessary to make your level challenging.
+     * 
+     * Use the createPlatform Function to create platforms for the level. 
+     * 
+     * createPlatform() takes these arguments:
+     *      
+     *      createPlatform(x, y, scaleX, scaleY);
+     * 
+     *      x: The x coordineate for the platform.
+     *      y: The y coordineate for the platform.
+     *      scaleX: OPTIONAL The scale factor on the x-axis, this value will 
+     *              stretch the platform in width.
+     *      scaleY: OPTIONAL The scale factor on the y-axis, this value will 
+     *              stretch the platform in height.
+     */ 
+    function init(game) {
+        let createPlatform = platform.create;
+
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        /*
+         * ground : here, we create a floor. Given the width of of the platform 
+         * asset, giving it a scaleX and scaleY of 2 will stretch it across the 
+         * bottom of the game.
+         */
+        createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
+
+        // example:
+        createPlatform(500,200, 0.4);//6th
+        createPlatform(550, 500, 0.5);//2nd
+        createPlatform(220, 580, 0.5);// 1st
+        createPlatform(220, 410, 0.8);// 3rd
+        createPlatform(50, 330, 0.5);// 4th
+        createPlatform(300, 250, 0.4);//5th
+        createPlatform(700,150, 0.4);//final
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    }
+    platform.init = init;
+})(window);
