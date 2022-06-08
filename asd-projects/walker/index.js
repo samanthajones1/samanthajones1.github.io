f27be5af610f47ebcd10a07addc18acc822d49ae
samanthajones1 <sammy.l.j2004@gmail.com>
2022-06-08 15:25:34 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -13,10 +13,46 @@ function runProgram(){
   
   // Game Item Objects
 
+      //TODO 3 -- declare key varible with object -- as well ad the keys and their numbers 
+    var PLAYER1 = {
+        LEFT: 37,
+        UP: 38,
+        RIGHT: 39, 
+        DOWN: 40,
+      };
+  
+      var PLAYER2 = {
+        A: 65,
+        W: 87,
+        D: 68, 
+        S: 83,
+      };
+      
+      //TODO 4
+
+      //POSITIONS 
+      var PLAYER1X = 0; // the x-coordinate 
+      var PLAYER1Y = 0; // the y-coordinate 
+
+      //SPEED 
+      var PLAYER1speedX ; // the x-axis
+      var PLAYER1speedY ; // the y-axis
+
+    //POSITIONS 
+    var PLAYER2X = 0; // the x-coordinate 
+    var PLAYER2Y = 0; // the y-coordinate 
+
+    //SPEED 
+    var PLAYER2speedX ; // the x-axis
+    var PLAYER2speedY ; // the y-axis
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+
+  /* TODO 2 --- 2a and 2b - changing the names of even and the function */
+
+
+  $(document).on('keydown',handleKeyDown);                           // change 'eventType' to the type of event you want to handle  
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -27,21 +63,69 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+
+    //TODO 6 -- calling the update functions 
+    repositionWalker();
+    redrawWalker();
 
   }
   
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
 
+  /* TODO 2 --- changing the name of the function and adding console.log to check each key -- 2c */
+  function handleKeyDown(event) {
+    if (event.LEFT === PLAYER1.LEFT){ //TODO 3 -- getting the press function down for keys 
+      console.log('left pressed');
+    }
+    if (event.UP === PLAYER1.UP){
+      console.log('UP pressed');
+    }
+    if (event.RIGHT === PLAYER1.RIGHT){
+      console.log('RIGHT pressed');
+    }
+    if (event.DOWN === PLAYER1.DOWN){
+      console.log('DOWN pressed');
+    }
+
+      //TODO 6 -- moving 
+    if (event.LEFT === PLAYER1.LEFT) {
+      PLAYER1speedX = -5;
+    }
+    
+    if (event.UP === PLAYER1.UP) {
+      PLAYER1speedY = -5;
+    }
+    
+    if (event.RIGHT === PLAYER1.RIGHT) {
+      PLAYER1speedX = 5;
+    }
+    
+    if (event.DOWN === PLAYER1.DOWN) {
+      PLAYER1speedY = 5;
+    }
+
+    
   }
 
+ 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
+  //TODO 5 -- 5a - decalring functions 
+  function repositionWalker(){
+    PLAYER1X += PLAYER1speedX; // update the position of the box along the x-axis
+    PLAYER1Y += PLAYER1speedY; // update the position of the box along the y-axis
+  }
+
+  function redrawWalker(){
+
+    $("#walker").css("left", PLAYER1X);    // draw the box in the new location, positionX pixels away from the "left"
+    $("#walker").css("top", PLAYER1Y);    // draw the box in the new location, positionX pixels away from the "top"
+
+  }
   
   function endGame() {
     // stop the interval timer
